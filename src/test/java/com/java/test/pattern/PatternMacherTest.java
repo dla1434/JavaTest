53f696b71c4be897981a195d5691d8f4034b6c66
dla1434 <dla1434@naver.com>
2018-06-26 17:30:01 +0900

    Add PatternMacherTest

---
+++ src/test/java/com/java/test/pattern/PatternMacherTest.java
@@ -0,0 +1,54 @@
+package com.java.test.pattern;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class PatternMacherTest {
+	@Test
+	public void gramIndextest() {
+//		Pattern p = Pattern.compile("\\{yyyy-mm-dd\\}");
+		Pattern p = Pattern.compile("\\$\\{.*\\}");
+		Matcher matcher = p.matcher("gram-prod-${yyyy-MM-dd}");
+		
+		boolean match = matcher.matches();
+		log.info("Anything is matching : {}", match);
+		
+		boolean findResult = matcher.find();
+		log.info("findResult : {}", findResult);
+		if( findResult ){
+			String rollingFormat = matcher.group().replaceAll("\\$\\{","").replaceAll("\\}", "");
+			log.info("rollingFormat : {}", findResult, rollingFormat);
+			DateTimeFormatter formatter = DateTimeFormatter.ofPattern(rollingFormat);
+			String rolling = LocalDateTime.now().format(formatter);
+			
+			String indexName = matcher.replaceFirst(rolling);
+			log.info("indexName : {}", indexName);
+		}
+		
+		String repalce = matcher.replaceAll("Test");
+		log.info("repalce : {}", repalce);
+		
+		
+		log.info(matcher.start() + " " + matcher.group());
+	}
+	
+	@Test
+	@Ignore
+	public void test() {
+		Pattern p = Pattern.compile("BAB");
+		Matcher m = p.matcher("ABCABABABCCCAA");
+
+		boolean a = false;
+		while(a = m.find()){
+			log.info(m.start() + " " + m.group());
+		}
+	}
+}
