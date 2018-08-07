6541550badf7da0f823619954b75ca8f1fac1dbc
dla1434 <dla1434@naver.com>
2018-08-07 18:06:08 +0900

    skip

--- src/test/java/com/java8/filter/Filter_FindAny_orElse.java
+++ src/test/java/com/java8/filter/Filter_FindAny_orElse.java
@@ -28,8 +28,8 @@ public class Filter_FindAny_orElse {
 	}
 
 	@Test
-	public void test1Classic() {
-		log.info(CommonConst.title, "testClassic");
+	public void test1_Classic() {
+		log.info(CommonConst.title, "test1_Classic");
 		Person result = null;
 		for (Person temp : persons) {
 			if ("jack".equals(temp.getName())) {
@@ -43,8 +43,10 @@ public class Filter_FindAny_orElse {
 	}
 	
 	@Test
-	public void test2FilterFound(){
-		log.info(CommonConst.title, "testFilterFound");
+	//findAny를 선언하면 조건에 맞는 결과를 찾으면 바로 return 시켜버린다.
+	//여러 개의 결과를 반환하는게 아닌 하나의 결과를 반환하기 때문이다.
+	public void test2_FilterFound(){
+		log.info(CommonConst.title, "test2_FilterFound");
 		Person result1 = persons.stream() 					// Convert to steam
 			.filter(x -> "jack".equals(x.getName())) 		// we want "jack" only
 			.findAny() 										// If 'findAny' then return found
@@ -55,8 +57,8 @@ public class Filter_FindAny_orElse {
 	}
 	
 	@Test
-	public void test3FilterNotFound(){
-		log.info(CommonConst.title, "testFilterNotFound");
+	public void test3_FilterNotFound(){
+		log.info(CommonConst.title, "test3_FilterNotFound");
 		Person result2 = persons.stream()
 			.filter(x -> "ahmook".equals(x.getName()))
 			.findAny()
@@ -66,8 +68,33 @@ public class Filter_FindAny_orElse {
 	}
 	
 	@Test
-	public void testFilter3(){
+	public void test4_FilterMultiCondition(){
+		log.info(CommonConst.title, "test4FilterMultiCondition");
+		Person result1 = persons.stream()
+			.filter((p) -> "jack".equals(p.getName()) && 20 == p.getAge())
+			.findAny()
+			.orElse(null);
+
+		System.out.println("result 1 :" + result1);
+		log.info(CommonConst.line);
+	}
+	
+	
+	@Test
+	//test4_FilterMultiCondition과 동일한 처리인데..-> {로 멀티라인으로 선언한 방식이다.
+	public void test5_FilterMultiCondition(){
+		log.info(CommonConst.title, "test5_FilterMultiCondition");
+		//or like this
+		Person result2 = persons.stream().filter(p -> {
+			if ("jack".equals(p.getName()) && 20 == p.getAge()) {
+				return true;
+			}
+			return false;
+		}).findAny().orElse(null);
 		
+		System.out.println("result2 :" + result2);
+		
+		log.info(CommonConst.line);
 	}
 
 }
