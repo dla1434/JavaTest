79403006d7be8149f9262ce6bd6c8a3f323e7138
dla1434 <dla1434@naver.com>
2018-08-10 14:52:57 +0900

    skip

---
+++ src/test/java/apache/common/ArraysUtilsTest.java
@@ -0,0 +1,94 @@
+package apache.common;
+
+
+import java.util.Arrays;
+
+import org.apache.commons.lang3.ArrayUtils;
+import org.junit.Before;
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runners.MethodSorters;
+
+import com.java.test.consts.CommonConst;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class ArraysUtilsTest {
+	@Before
+	public void init(){
+		
+	}
+	
+	@Test
+	public void test1_Classic() {
+		log.info(CommonConst.title, "test1_Classic");
+		log.info(CommonConst.line);
+	}
+	
+	@Test
+	public void test2_ApacheCommon_ArrayUtils_StringArr(){
+		log.info(CommonConst.title, "test2_ApacheCommon_ArrayUtils_StringArr");
+		String[] s1 = new String[] { "a", "b", "c" };
+		String[] s2 = new String[] { "d", "e", "f" };
+
+		String[] result = ArrayUtils.addAll(s1, s2);
+		System.out.println(Arrays.toString(result));
+
+		log.info(CommonConst.line);
+	}
+	
+	@Test
+	public void test3_ApacheCommon_ArrayUtils_IntArr(){
+		log.info(CommonConst.title, "test3_ApacheCommon_ArrayUtils_IntArr");
+		int[] int1 = new int[] { 1, 2, 3 };
+		int[] int2 = new int[] { 4, 5, 6 };
+		
+		int[] result2 = ArrayUtils.addAll(int1, int2);
+		System.out.println(Arrays.toString(result2));
+		log.info(CommonConst.line);
+	}
+	
+	@Test
+	public void test4_(){
+		log.info(CommonConst.title, "test4_");
+		log.info(CommonConst.line);
+	}
+	
+	@Test
+	public void test5_(){
+		log.info(CommonConst.title, "test5_");
+		log.info(CommonConst.line);
+	}
+	
+	@Test
+	public void test6_(){
+		log.info(CommonConst.title, "test6_");
+		log.info(CommonConst.line);
+	}
+	
+	@Test
+	public void test7_(){
+		log.info(CommonConst.title, "test7_");
+		log.info(CommonConst.line);
+	}
+	
+	@Test
+	public void test8_(){
+		log.info(CommonConst.title, "test8_");
+		log.info(CommonConst.line);
+	}
+	
+	@Test
+	public void test9_(){
+		log.info(CommonConst.title, "test9_");
+		log.info(CommonConst.line);
+	}
+	
+	@Test
+	public void test10_(){
+		log.info(CommonConst.title, "test10_");
+		log.info(CommonConst.line);
+	}
+}
--- src/test/java/com/java8/files/FilesTest.java
+++ src/test/java/com/java8/files/FilesTest.java
@@ -1,10 +1,17 @@
 package com.java8.files;
 
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import org.junit.Before;
@@ -25,22 +32,45 @@ public class FilesTest {
 	}
 	
 	@Test
-	public void test1_Classic() {
+	//new BufferedReader(new FileReader("파일")
+	public void test1_ClassicBufferedReader() {
 		log.info(CommonConst.title, "test1_Classic");
+		try (BufferedReader br = new BufferedReader(new FileReader(getClass().getResource("lines.txt").getPath().substring(1)))) {
+			String line;
+			while ((line = br.readLine()) != null) {
+				System.out.println(line);
+			}
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 		log.info(CommonConst.line);
 	}
 	
 	@Test
-	public void test2_(){
-		log.info(CommonConst.title, "test2_");
+	public void test1_ClassicScanner() {
+		log.info(CommonConst.title, "test1_Classic");
+		try (Scanner scanner = new Scanner(new File(getClass().getResource("lines.txt").getPath().substring(1)))) {
+			while (scanner.hasNext()){
+				System.out.println(scanner.nextLine());
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		log.info(CommonConst.line);
+	}
+	
+	@Test
+	public void test2_FileRead_Stream(){
+		log.info(CommonConst.title, "test2_FileRead_Stream");
 		//절대 경로
 //		String fileName = "c://lines.txt";
 		
 		//프로젝트 최상단에 위치 시
 //		String fileName = "lines.txt";
 		
-		//현재 패키지에 위치 시
-		String fileName = "./lines.txt";
+		//현재 패키지에 위치 시 - 실패
+//		String fileName = "./lines.txt";
 		
 		//실패) 절대 경로는 획득됐지만..앞에 /가 붙어서 인식이 안 된다. 이 방법을 사용하고 싶다면..substring으로 /를 제거하자
 		URL url = getClass().getResource("lines.txt");
@@ -58,14 +88,39 @@ public class FilesTest {
 	}
 	
 	@Test
-	public void test3_(){
+	public void test3_FileRead_Stream_Extra(){
 		log.info(CommonConst.title, "test3_");
+		List<String> list = new ArrayList<>();
+
+		//1. filter line 3
+		//2. convert all content to upper case
+		//3. convert it into a List
+		try (Stream<String> stream = Files.lines(Paths.get(getClass().getResource("lines.txt").getPath().substring(1)))) {
+			list = stream
+					.filter(line -> !line.startsWith("line3"))
+					.map(String::toUpperCase)
+					.collect(Collectors.toList());
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		list.forEach(System.out::println);
 		log.info(CommonConst.line);
 	}
 	
 	@Test
-	public void test4_(){
+	public void test4_BufferReader_Stream(){
 		log.info(CommonConst.title, "test4_");
+		List<String> list = new ArrayList<>();
+		try (BufferedReader br = Files.newBufferedReader(Paths.get(getClass().getResource("lines.txt").getPath().substring(1)))) {
+			//br returns as stream and convert it into a List
+			list = br.lines().collect(Collectors.toList());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	
+		list.forEach(System.out::println);
 		log.info(CommonConst.line);
 	}
 	
